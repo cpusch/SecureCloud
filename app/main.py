7d18967bbf744d4464204f4222ef531ab4ba01a2
cpusch <conpusch@yahoo.com>
2023-04-16 21:30:31 +0100

    added file verification

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from admin import add_user, remove_user
+from connect_to_cloud import download_assets_and_verify
 
 def generate_key_pair() -> tuple:
     key = rsa.generate_private_key(
@@ -22,41 +24,47 @@ def generate_key_pair() -> tuple:
 
 def main():
     print("Welcome to the Secure Cloud!")
-    while True:
-        print("""
-    Please Choose a following Option (You can type 'home' at any time to return to this prompt or 'quit' to exit): 
+    admin_public_key = input("Please enter the admins public key to verify files: ")
+    if download_assets_and_verify():
+        while True:
+            print("""
+        Please Choose a following Option (You can type 'home' at any time to return to this prompt or 'quit' to exit): 
 
-    1. Generate a key pair
-    2. Upload a File
-    3. Download a File
-    4. Admin
-    """)
-        user_selection = input("Select 1,2,3, or 4: ")
+        1. Generate a key pair
+        2. Upload a File
+        3. Download a File
+        4. Admin
+        """)
+            user_selection = input("Select 1,2,3, or 4: ")
 
-        if user_selection == '1':
-            key_pair = generate_key_pair()
-            print("Here is your private/public key pair. Keep your private key safe!\n")
-            print(f"{key_pair[0].decode()}\n")
-            print(f"{key_pair[1].decode()}\n")
+            if user_selection == '1':
+                key_pair = generate_key_pair()
+                print("Here is your private/public key pair. Keep your private key safe!\n")
+                print(f"{key_pair[0].decode()}\n")
+                print(f"{key_pair[1].decode()}\n")
 
-        elif user_selection == '2':
-            upload_file()
-        elif user_selection == '3':
-            download_file()
-        elif user_selection == '4':
-            while True:
-                add_or_remove = input("\nWould you like to 'add' or 'remove' a user? ").lower()
-                if add_or_remove == 'add':
-                    username = input("Username: ")
-                    user_public_key = input("Public Key: ")
-                    add_user(username, user_public_key)
-                elif add_or_remove == 'remove':
-                    remove_user()
-                elif add_or_remove == 'home':
-                    break
-                else:
-                    print("Please enter a valid prompt")
-
+            elif user_selection == '2':
+                upload_file()
+            elif user_selection == '3':
+                download_file()
+            elif user_selection == '4':
+                while True:
+                    add_or_remove = input("\nWould you like to 'add' or 'remove' a user? ").lower()
+                    if add_or_remove == 'add':
+                        username = input("Username: ")
+                        user_public_key = input("Public Key: ")
+                        add_user(username, user_public_key)
+                    elif add_or_remove == 'remove':
+                        username = input("Username: ")
+                        remove_user(username)
+                    elif add_or_remove == 'home':
+                        break
+                    else:
+                        print("Please enter a valid prompt")
+            elif user_selection == 'quit':
+                break
+    else: 
+        print("Files not signed by admin. ")
 
 if __name__ == "__main__":
     main()

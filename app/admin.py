c693fedbe532908cd1439134fb151bab15d5654c
cpusch <conpusch@yahoo.com>
2023-04-16 21:29:49 +0100

    implemented remove user

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 import sqlite3
 from cryptography.hazmat.primitives import serialization,hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-from CONSTANTS import DB_PATH,RANDOM_KEY
+from CONSTANTS import *
+
 
 def search_db(query):
     connection = sqlite3.connect(DB_PATH)
@@ -12,10 +13,10 @@ def search_db(query):
     return result
 
 
-
 def add_user(username:str,user_public_key:str): 
     user_public_key = serialization.load_pem_public_key(user_public_key.encode())
     
+    # encrypts the symmetric decryption key with the new users public key
     ENCRYPTED_SYMMETRIC_DECRYPTION_KEY = user_public_key.encrypt(
         RANDOM_KEY,
         padding.OAEP(
@@ -31,7 +32,12 @@ def add_user(username:str,user_public_key:str):
     cursor.execute(f'INSERT INTO Users (USERNAME,PUBLIC_KEY,ENCRYPTED_SYMETRIC_DYCRYPTION_KEY) VALUES (?,?,?)',to_insert)
     connection.commit()
 
-def remove_user(username):
+def remove_user(username,):
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute(f'DELETE FROM Users WHERE USERNAME = {username};')
+    connection.commit()
 
-
+def _reset_keys(admin_private_key:str):
+    return
 

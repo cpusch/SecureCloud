f95850212a02cda008284c6472d6592c16c54951
cpusch <conpusch@yahoo.com>
2023-04-17 01:15:19 +0100

    updated add user so random key is not hardcoded

--- app/admin.py
+++ app/admin.py
@@ -14,19 +14,32 @@ def search_db(query):
     return result
 
 
-def add_user(username:str,user_public_key:str): 
-    user_public_key = serialization.load_pem_public_key(user_public_key.encode())
-    
-    # encrypts the symmetric decryption key with the new users public key
-    ENCRYPTED_SYMMETRIC_DECRYPTION_KEY = user_public_key.encrypt(
-        RANDOM_KEY,
+def add_user(username:str,user_public_key:str,admin_private_key:str): 
+    loaded_user_public_key = serialization.load_pem_public_key(user_public_key.encode())
+
+    admin_loaded_key = serialization.load_pem_private_key(admin_private_key.encode(),password=None)
+    admin_info = search_db(f"SELECT * FROM Users WHERE USERNAME = 'admin'")
+    random_key = admin_info[0][2]
+
+    random_key = admin_loaded_key.decrypt(
+        random_key,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     )
-    to_insert = (username,user_public_key,ENCRYPTED_SYMMETRIC_DECRYPTION_KEY)
+
+    # encrypts the symmetric decryption key with the new users public key
+    ENCRYPTED_SYMMETRIC_DECRYPTION_KEY = loaded_user_public_key.encrypt(
+        random_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    to_insert = (username,user_public_key.encode(),ENCRYPTED_SYMMETRIC_DECRYPTION_KEY)
 
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
@@ -38,7 +51,7 @@ def add_user(username:str,user_public_key:str):
 def remove_user(username:str):
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
-    cursor.execute(f'DELETE FROM Users WHERE USERNAME = {username};')
+    cursor.execute(f"DELETE FROM Users WHERE USERNAME = '{username}';")
     connection.commit()
     connection.close()
     _reset_keys()
@@ -46,7 +59,7 @@ def remove_user(username:str):
 def _reset_keys() -> None:
     # encrypts the random key with users public key
     def encrypt_random_key(user_public_key:bytes, random_key) -> bytes:
-        user_public_key = serialization.load_pem_public_key(user_public_key.encode())
+        user_public_key = serialization.load_pem_public_key(user_public_key)
         return user_public_key.encrypt(
             random_key,
             padding.OAEP(
@@ -58,19 +71,24 @@ def _reset_keys() -> None:
 
     # generate new keys and write them to encrypted key file
     new_encryption_key = Fernet.generate_key()
+    print(f"Encryption key: {new_encryption_key}")
     new_random_encryption_key = Fernet.generate_key()
+    print(f"Random Key: {new_random_encryption_key}")
     encrypted_encryption_key = Fernet(new_random_encryption_key).encrypt(new_encryption_key)
     with open(ENCRYPTED_KEY,'wb') as file:
         file.write(encrypted_encryption_key)
 
     # get all users and update their ENCRYPTED_SYMETRIC_DYCRYPTION_KEY in db
     users = search_db('SELECT * FROM USERS')
+    updated_users = []
     for user in users:
-        user[2] = encrypt_random_key(user[1],new_random_encryption_key)
+        updated_random_key = encrypt_random_key(user[1],new_random_encryption_key)
+        updated_users.append((user[0],user[1],updated_random_key))
 
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
-    cursor.executemany(f'INSERT INTO Users (USERNAME, PUBLIC_KEY, ENCRYPTED_SYMETRIC_DYCRYPTION_KEY) VALUES (?, ?, ?)',users)
+    cursor.execute(f'delete from Users')
+    cursor.executemany(f'INSERT INTO Users (USERNAME, PUBLIC_KEY, ENCRYPTED_SYMETRIC_DYCRYPTION_KEY) VALUES (?, ?, ?)',updated_users)
     connection.commit()
     connection.close()
     
@@ -114,7 +132,7 @@ def sign_db_and_key(admin_private_key:str) -> None:
     with open(ADMIN_DB_SIGNATURE,'wb') as file:
         file.write(db_sig)
     with open(ADMIN_KEY_SIGNATURE, 'wb') as file:
-        file.read(key_sig)    
+        file.write(key_sig)    
     
     return
 
